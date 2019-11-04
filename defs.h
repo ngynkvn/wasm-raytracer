49563858299c45aa1557f4919861fc96546b3fa1
Kevin Nguyen <ngynkvn@gmail.com>
2019-11-04 00:14:04 -0500

    Comments

---
+++ defs.h
@@ -0,0 +1,78 @@
+#pragma once
+
+/**
+*   Simple 3-d point / vector struct with operations implemented for 
+*   addition, subtraction, dot product, and scaling.
+*/
+struct Point {
+  double x;
+  double y;
+  double z;
+  Point(double x, double y, double z) : x(x), y(y), z(z) {}
+  Point() : x(0), y(0), z(0) {}
+  Point operator+(const Point &other) {
+    return Point(x + other.x, y + other.y, z + other.z);
+  }
+
+  Point operator-(const Point &other) {
+    return Point(x - other.x, y - other.y, z - other.z);
+  }
+
+  double operator*(const Point &other) {
+    return x * other.x + y * other.y + z * other.z;
+  }
+
+  double length() { return std::sqrt((*this) * (*this)); }
+
+  Point operator/(double value) {
+    return Point(x / value, y / value, z / value);
+  }
+  Point operator*(double value) {
+    return Point(x * value, y * value, z * value);
+  }
+};
+std::ostream &operator<<(std::ostream &os, const Point &p) {
+  os << "<" << p.x << ", " << p.y << ", " << p.z << ">";
+  return os;
+}
+
+/**
+* Color of RGB values
+*/
+struct Color {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  Color(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {}
+  Color operator*(double value) {
+    return Color(r * value, g * value, b * value);
+  }
+};
+/**
+* Color constants
+*/
+const Color WHITE(255, 255, 255);
+const Color RED(255, 0, 0);
+const Color GREEN(0, 255, 0);
+const Color BLUE(0, 0, 255);
+
+enum LightType { AMBIENT, POINT, DIRECTIONAL };
+struct Light {
+  Point vector;
+  double intensity;
+  LightType type;
+  Light(Point vector, double intensity, LightType type)
+      : vector(vector), intensity(intensity), type(type) {}
+};
+
+/**
+* Sphere defined by a center and a radius.
+* Color attribute is used for render
+*/
+struct Sphere {
+  Point center;
+  double radius;
+  Color color;
+  Sphere(Point center, double radius, Color color)
+      : center(center), radius(radius), color(color) {}
+};
--- main.cpp
+++ main.cpp
@@ -6,91 +6,55 @@
 #include <png++/png.hpp>
 #include <utility>
 #include <vector>
+#include "defs.h"
 
-struct Point {
-  double x;
-  double y;
-  double z;
-  Point(double x, double y, double z) : x(x), y(y), z(z) {}
-  Point() : x(0), y(0), z(0) {}
-  Point operator+(const Point &other) {
-    return Point(x + other.x, y + other.y, z + other.z);
-  }
-
-  Point operator-(const Point &other) {
-    return Point(x - other.x, y - other.y, z - other.z);
-  }
-
-  double operator*(const Point &other) {
-    return x * other.x + y * other.y + z * other.z;
-  }
-
-  double length() { return std::sqrt((*this) * (*this)); }
-
-  Point operator/(double value) {
-    return Point(x / value, y / value, z / value);
-  }
-  Point operator*(double value) {
-    return Point(x * value, y * value, z * value);
-  }
-};
-std::ostream &operator<<(std::ostream &os, const Point &p) {
-  os << "<" << p.x << ", " << p.y << ", " << p.z << ">";
-  return os;
-}
-
-struct Color {
-  int r;
-  int g;
-  int b;
-  Color(int r, int g, int b) : r(r), g(g), b(b) {}
-  Color operator*(double value) {
-    return Color(r * value, g * value, b * value);
-  }
-};
-const Color white(255, 255, 255);
-
-enum LightType { AMBIENT, POINT, DIRECTIONAL };
-struct Light {
-  Point vector;
-  double intensity;
-  LightType type;
-  Light(Point vector, double intensity, LightType type)
-      : vector(vector), intensity(intensity), type(type) {}
-};
-
-struct Sphere {
-  Point origin;
-  double radius;
-  Color color;
-  Sphere(Point origin, double radius, Color color)
-      : origin(origin), radius(radius), color(color) {}
-};
-
+Point CameraPosition = Point(0, 0, 0);
+int Cw = 2000;
+int Ch = 2000;
+int Vw = 1;
+int Vh = 1;
+int z_dist = 1;
+/**
+* Definitions of the scene.
+* This is what is actually being rendered.
+*/
 struct Scene {
   std::vector<Sphere> spheres;
   std::vector<Light> lights;
   Scene() {
-    spheres.emplace_back(Point(0, -1, 3), 1, Color(255, 0, 0));
-    spheres.emplace_back(Point(2, 0, 4), 1, Color(0, 255, 0));
-    spheres.emplace_back(Point(-2, 0, 4), 1, Color(0, 0, 255));
-    spheres.emplace_back(Point(0, -5001, 0), 5000, Color(0, 255, 255));
+    spheres.emplace_back(Point(0, -1, 3), 1, RED);
+    spheres.emplace_back(Point(2, 0, 4), 1, GREEN);
+    spheres.emplace_back(Point(-2, 0, 4), 1, BLUE);
+    spheres.emplace_back(Point(0, -5001, 0), 5000, Color(0, 255, 255));//Teal?
     lights.emplace_back(Point(0, 0, 0), .2, AMBIENT);
     lights.emplace_back(Point(2, 1, 0), .6, POINT);
     lights.emplace_back(Point(1, 4, 4), .2, DIRECTIONAL);
+    std::cout << "Initialized scene with " 
+              << spheres.size() << " spheres, "
+              << lights.size() << " lights."
+              << std::endl;
   }
 };
 Scene scene;
 
+/**
+* Solves the quadratic formula of where a ray intersects the surface of a sphere.
+*
+* Derivation comes from:
+*
+* radius^2 = |P - center| * |P - center| => <P-center, P-center>
+* P = origin + dir*t
+*
+* Plugging p into radius eq and solving for t 
+* gives a value t that equals points on surface of the sphere.
+*/
 std::pair<double, double> intersect_ray_sphere(Point &origin, Point &dir,
                                                Sphere &sphere) {
-  auto center = sphere.origin;
-  auto radius = sphere.radius;
-  auto oc = origin - center;
+  auto oc = origin - sphere.center;
 
   auto k1 = dir * dir;
   auto k2 = 2.0 * (oc * dir);
-  auto k3 = oc * oc - radius * radius;
+  auto k3 = oc * oc - sphere.radius * sphere.radius;
 
   auto discriminant = k2 * k2 - 4.0 * k1 * k3;
   if (discriminant < 0.0) {
@@ -102,6 +66,13 @@ std::pair<double, double> intersect_ray_sphere(Point &origin, Point &dir,
   }
 }
 
+/**
+* Computation of the light vectors is done by computing
+* normal vectors of the surface of the sphere and then
+* taking the dot product 
+* with the direction from the light source.
+* Depending on light source type, adjust vector as appropriate.
+*/
 double compute_lighting(Point P, Point N) {
   auto i = 0.0;
   for (auto &l : scene.lights) {
@@ -123,6 +94,12 @@ double compute_lighting(Point P, Point N) {
   return i;
 }
 
+/**
+* Simulate a single ray from point extending outwards in dir.
+* Returns the color for the specific square.
+* This is the algorithm that outputs the colors 
+* necessary to create the image.
+*/
 Color trace_ray(Point &origin, Point &dir, double t_min, double t_max) {
   auto t = DBL_MAX;
   std::optional<Sphere> m;
@@ -141,20 +118,19 @@ Color trace_ray(Point &origin, Point &dir, double t_min, double t_max) {
   }
   if (m) {
     auto P = origin + (dir * t);
-    auto N = P - m.value().origin;
+    auto N = P - m.value().center;
     N = N / N.length();
     return m.value().color * compute_lighting(P, N);
   }
-  return white;
+  return WHITE;
 }
 
+/**
+* Create the canvas, a scaling function, 
+* then iterate over each pixel 
+* and trace the path of ray from camera towards the viewport.
+*/
 int main() {
-  auto CameraPosition = Point(0, 0, 0);
-  auto Cw = 200;
-  auto Ch = 200;
-  auto Vw = 1;
-  auto Vh = 1;
-  auto z_dist = 1;
   png::image<png::rgb_pixel> image(Cw, Ch);
   auto canvas_to_viewport = [&](double x, double y) {
     return Point(x * Vw / Cw, y * Vh / Ch, z_dist);
