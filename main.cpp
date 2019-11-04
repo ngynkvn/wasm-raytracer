95159e506e2243c6fe34f39b65b175d9baa74877
Kevin Nguyen <ngynkvn@gmail.com>
2019-11-03 20:26:06 -0500

    Compute lighting

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <algorithm>
 #include <cfloat>
 #include <cmath>
+#include <iostream>
+#include <png++/png.hpp>
 #include <vector>
+#include <utility>
+#include <optional>
 
 struct Point {
   double x;
   double y;
   double z;
   Point(double x, double y, double z) : x(x), y(y), z(z) {}
+  Point() : x(0), y(0), z(0) {}
   Point operator+(const Point &other) {
     return Point(x + other.x, y + other.y, z - other.z);
   }
@@ -19,16 +24,61 @@ struct Point {
   double operator*(const Point &other) {
     return x * other.x + y * other.y + z * other.z;
   }
+
+  double length() { return std::sqrt((*this) * (*this)); }
+
+  Point operator/(double value) {
+    return Point(x / value, y / value, z / value);
+  }
+  Point operator*(double value) {
+    return Point(x * value, y * value, z * value);
+  }
+};
+
+struct Color {
+  int r;
+  int g;
+  int b;
+  Color(int r, int g, int b) : r(r), g(g), b(b) {}
+  Color operator*(double value) {
+    return Color(r * value, g * value, b * value);
+  }
+};
+const Color white(255, 255, 255);
+
+enum LightType { AMBIENT, POINT, DIRECTIONAL };
+struct Light {
+  Point vector;
+  double intensity;
+  LightType type;
+  Light(Point vector, double intensity, LightType type)
+      : vector(vector), intensity(intensity), type(type) {}
 };
 
 struct Sphere {
   Point origin;
   double radius;
-  int color;
-  Sphere(Point origin, double radius, int color) : origin(origin), radius(radius), color(color) {}
+  Color color;
+  Sphere(Point origin, double radius, Color color)
+      : origin(origin), radius(radius), color(color) {}
 };
 
-double intersect_ray_sphere(Point &origin, Point &dir, Sphere &sphere) {
+struct Scene {
+  std::vector<Sphere> spheres;
+  std::vector<Light> lights;
+  Scene() {
+    spheres.emplace_back(Point(0, -1, 3), 1, Color(255, 0, 0));
+    spheres.emplace_back(Point(2, 0, 4), 1, Color(0, 255, 0));
+    spheres.emplace_back(Point(-2, 0, 4), 1, Color(0, 0, 255));
+    spheres.emplace_back(Point(0, -5001, 0), 5000, Color(0, 255, 255));
+    lights.emplace_back(Point(0, 0, 0), .2, AMBIENT);
+    lights.emplace_back(Point(2, 1, 0), .6, POINT);
+    lights.emplace_back(Point(1, 4, 4), .2, DIRECTIONAL);
+  }
+};
+Scene scene;
+
+std::pair<double, double> intersect_ray_sphere(Point &origin, Point &dir, Sphere &sphere) {
   auto center = sphere.origin;
   auto radius = sphere.radius;
   auto oc = origin - center;
@@ -39,47 +89,80 @@ double intersect_ray_sphere(Point &origin, Point &dir, Sphere &sphere) {
 
   auto discriminant = k2 * k2 - 4.0 * k1 * k3;
   if (discriminant < 0.0) {
-    return DBL_MAX;
+    return std::make_pair(DBL_MAX, DBL_MAX);
   } else {
     auto t1 = (-k2 + std::sqrt(discriminant)) / (2.0 * k1);
     auto t2 = (-k2 - std::sqrt(discriminant)) / (2.0 * k1);
-    return std::min(t1, t2);
+    return std::make_pair(t1, t2);
   }
 }
 
-int trace_ray(Point &origin, Point &dir, double t_min, double t_max,
-              std::vector<Sphere> &spheres) {
-  auto intersect = [&](Sphere &a, Sphere &b) {
-    return intersect_ray_sphere(origin, dir, a) <
-           intersect_ray_sphere(origin, dir, b);
-  };
-  Sphere m = *std::min_element(spheres.begin(), spheres.end(), intersect);
-  if (intersect_ray_sphere(origin, dir, m) != DBL_MAX) {
-    return m.color;
+double compute_lighting(Point P, Point N) {
+  auto i = 0.0;
+  for (auto &l : scene.lights) {
+    if (l.type == AMBIENT) {
+      i += l.intensity;
+    } else {
+      Point L;
+      if (l.type == POINT) {
+        L = l.vector - P;
+      } else if (l.type == DIRECTIONAL) {
+        L = l.vector;
+      }
+      double dot = N * L;
+      if (dot > 0) {
+        i += (l.intensity * dot) / (N.length() * L.length());
+      }
+    }
   }
-  return 0xFFFFFF;
+  return i;
+}
+
+Color trace_ray(Point &origin, Point &dir, double t_min, double t_max) {
+  auto t = DBL_MAX;
+  std::optional<Sphere> m;
+  for (auto &s : scene.spheres) {
+    auto t_s = intersect_ray_sphere(origin, dir, s);
+    auto fst = t_s.first;
+    auto snd = t_s.second;
+    if (fst < t && t_min < fst && fst < t_max) { 
+      t = fst;
+      m = s;
+    }
+    if (snd < t && t_min < snd && snd < t_max) { 
+      t = snd;
+      m = s;
+    }
+  }
+  if (m) {
+    auto P = origin + dir * t;
+    auto N = P - m.value().origin;
+    N = N / N.length();
+    return m.value().color * compute_lighting(P, N);
+  }
+  return white;
 }
 
 int main() {
   auto CameraPosition = Point(0, 0, 0);
-  auto Cw = 400;
-  auto Ch = 400;
+  auto Cw = 800;
+  auto Ch = 800;
   auto Vw = 1;
   auto Vh = 1;
   auto z_dist = 1;
-  int canvas[Ch][Cw];
-  std::vector<Sphere> spheres;
-  spheres.emplace_back(Point(0, -1, 3), 1, 0xFF0000);
-  spheres.emplace_back(Point(2, 0, 4), 1, 0x00FF00);
-  spheres.emplace_back(Point(-2, 0, 4), 1, 0x0000FF);
-  auto canvas_to_viewport = [&](int x, int y) {
+  png::image<png::rgb_pixel> image(Cw, Ch);
+  auto canvas_to_viewport = [&](double x, double y) {
     return Point(x * Vw / Cw, y * Vh / Ch, z_dist);
   };
-  for (int x = -Vw / 2; x < Vw / 2; x++) {
-    for (int y = -Vh / 2; y < Vh / 2; y++) {
+  auto CwO = Cw / 2;
+  auto ChO = Ch / 2;
+  for (int x = -Cw / 2; x < Cw / 2; x++) {
+    for (int y = -Ch / 2; y < Ch / 2; y++) {
       auto dir = canvas_to_viewport(x, y);
-      canvas[y][x] = trace_ray(CameraPosition, dir, 0, 1000, spheres);
+      auto color = trace_ray(CameraPosition, dir, 0, 2000);
+      image[ChO - y - 1][x + CwO] = png::rgb_pixel(color.r, color.g, color.b);
     }
   }
+  image.write("test.png");
   return 0;
 }
