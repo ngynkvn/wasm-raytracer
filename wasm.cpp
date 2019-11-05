fe3ae4f8f6dde9ecb23cc6dd4f5d814e8b47cbba
Kevin Nguyen <ngynkvn@gmail.com>
2019-11-05 15:14:06 -0500

    Works with wasm now

--- wasm.cpp
+++ wasm.cpp
@@ -1,16 +1,17 @@
 #include "defs.h"
 #include "raytracer.h"
+#include "scene_parser.h"
 #include <emscripten/bind.h>
 #include <string>
 #include <vector>
-
 using namespace emscripten;
-
-Scene scene_from_str(const std::string &str);
 Point canvas_to_viewport(double x, double y) {
-  return Point(x * Vw / Cw, y * Vh / Ch, z_dist);
+    return Point(x * Vw / Cw, y * Vh / Ch, z_dist);
+  };
+Scene scene_from_str(const std::string &str) {
+  SceneParser sp(str);
+  return sp.parse();
 };
-
 struct ColorStruct {
   double r;
   double g;
@@ -24,7 +25,7 @@ ColorStruct to_struct(Color c) {
   cs.b = c.b;
   return cs;
 }
-const Scene scene = {scene_camera, scene_spheres, scene_lights};
+const Scene scene;
 ColorStruct trace(double x, double y) {
   auto dir = canvas_to_viewport(x, y);
   return to_struct(trace_ray(scene, dir, 0, 2000));
@@ -65,7 +66,7 @@ val create_view() {
   return val(typed_memory_view(Cw * Ch * 4, buffer));
 }
 
-val create_view_from(const std::string &str) {
+val create_view_from(const std::string& str) {
   static uint8_t buffer[Cw * Ch * 4];
   size_t ptr = 0;
   Scene scene = scene_from_str(str);
@@ -83,13 +84,12 @@ val create_view_from(const std::string &str) {
 }
 
 EMSCRIPTEN_BINDINGS(m) {
-  function("create_vec", &create_vec);
   function("trace", &trace);
   function("create_view", &create_view);
   function("create_view_from", &create_view_from);
   value_array<ColorStruct>("Color")
       .element(&ColorStruct::r)
       .element(&ColorStruct::g)
-      .element(&ColorStruct::b);
+      .element(&ColorStruct::b);  
   register_vector<uint8_t>("ColorVec");
 }
